ce085d0150e1e0a3b212571b1f638a8bcafa7c35
D1360P3C0 <diegopeco1997@gmail.com>
2025-03-31 19:38:28 +0200

    Update process-script.js

--- pages/api/process-script.js
+++ pages/api/process-script.js
@@ -1,22 +1,18 @@
 // pages/api/process-script.js
 export default function handler(req, res) {
-  // Datos de ejemplo pre-definidos con estructura mínima
-  const exampleResults = {
+  // Respuesta básica para pruebas
+  res.status(200).json({ 
     results: [
       {
         scene: {
           location: "Cafetería",
-          timeOfDay: "Día",
-          visualElements: "Café, periódico, ventanas",
-          action: "Un hombre bebe café mientras lee el periódico"
+          timeOfDay: "Día"
         },
         stockContent: {
-          videos: [
-            {
-              id: 1,
-              image: "https://images.pexels.com/videos/3256029/free-video-3256029.jpg",
-              video_files: [
-                {
-                  link: "https://player.vimeo.com/external/372167359.hd.mp4?s=158696d441f48b2694957285f1a07b24dc7a5c6f&profile_id=175&oauth2_token_id=57447761",
-                  duration: 8.4
-                }
+          videos: [],
+          photos: []
+        }
+      }
+    ] 
+  });
+}
